3e2b2999f3c3c626c963935319da01c679dece2e
codeYongqi <zhuyongqi9@gmail.com>
2023-07-13 17:58:12 +0800

    add test.js

--- test.js
+++ test.js
@@ -1,2 +1,24 @@
-let str = "curl/7.81.0"
-console.log(str.split(""))
+
+const https = require('https');
+const url = 'https://www.arduino.cc'; // 替换为你的 URL
+const cheerio = require('cheerio')
+
+    https.get(url, (response) => {
+        let html = '';
+      
+        response.on('data', (chunk) => {
+          html += chunk;
+        });
+      
+        response.on('end', () => {
+        const $ = cheerio.load(html);
+        const title = $('title').text();
+const iconHref = $('link[rel="icon"]').attr('href');
+const description = $('meta[name="description"]').attr('content');
+console.log(title)
+console.log(iconHref)
+console.log(description)
+        });
+      }).on('error', (error) => {
+        console.error(error);
+      });
